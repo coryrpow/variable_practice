3bb0fca8e8833f4d1ba4499f872a5b53b6aa2d4a
Cory Powell <coryrpowell@gmail.com>
2023-05-14 22:01:40 -0400

    add float

--- variables.rb
+++ variables.rb
@@ -8,3 +8,13 @@ location = "philadelphia"
 age = 32
 zip_code = 19143
 future_age = 33
+
+# Float
+height_inches = 71.5
+neck_inches = 15.25
+shoulder_inches = 17.5
+
+#Boolean
+enrolled = true
+working_practice = true
+in_georgia_currently = false
