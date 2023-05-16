a8e12da6ac089dc8422ebaf32b7ed9494e3fb446
Cory Powell <coryrpowell@gmail.com>
2023-05-16 09:56:52 -0400

    Add puts

--- variables.rb
+++ variables.rb
@@ -4,11 +4,14 @@ first_name = "cory"
 last_name = "powell"
 location = "philadelphia"
 
+first_name = "frank"
+puts "Hello, I'm #{first_name}"
+
 # Integer
 age = 32
 zip_code = 19143
 future_age = 33
-
+puts age
 # Float
 height_inches = 71.5
 neck_inches = 15.25
@@ -18,3 +21,5 @@ shoulder_inches = 17.5
 enrolled = true
 working_practice = true
 in_georgia_currently = false
+
+puts enrolled
